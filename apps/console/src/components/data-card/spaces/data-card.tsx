6a92e8d92da81368f1f7996db904221e28f2111a
Kyle Wong <wongyeowkin@gmail.com>
2023-10-01 18:56:11 +0800

    chore: add focus state for space card

--- apps/console/src/components/data-card/spaces/data-card.tsx
+++ apps/console/src/components/data-card/spaces/data-card.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import Link from "next/link";
 import {
   Card,
   CardContent,
@@ -6,6 +7,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@dashbored/ui";
+import { cn } from "@dashbored/utils";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 
@@ -14,27 +16,44 @@ dayjs.extend(relativeTime);
 interface Props {
   title: string;
   description: string;
+  slug: string;
   updatedAt: Date;
   action?: React.ReactNode;
 }
 
-export function DataCard({ title, description, updatedAt, action }: Props) {
+export function DataCard({
+  title,
+  slug,
+  description,
+  updatedAt,
+  action,
+}: Props) {
   return (
-    <Card className="flex flex-col justify-between">
-      <CardHeader className="flex flex-row items-start gap-4 space-y-0">
-        <div className="flex-1 space-y-1">
-          <CardTitle>{title}</CardTitle>
-          <CardDescription className="line-clamp-3">
-            {description}
-          </CardDescription>
-        </div>
-        {action}
-      </CardHeader>
-      <CardContent>
-        <div className="text-muted-foreground flex space-x-4 text-xs">
-          <span>Last Updated {dayjs(updatedAt).fromNow()}</span>
-        </div>
-      </CardContent>
-    </Card>
+    <div className="relative">
+      <Link href={`./${slug}`}>
+        <Card
+          className={cn(
+            "hover:bg-accent hover:text-accent-foreground flex h-full flex-col justify-between transition-all duration-200 hover:shadow-md",
+            action && "pr-14",
+          )}
+        >
+          <CardHeader className="flex flex-row items-start gap-4 space-y-0">
+            <div className="flex-1 space-y-1">
+              <CardTitle>{title}</CardTitle>
+              <CardDescription className="line-clamp-3">
+                {description}
+              </CardDescription>
+            </div>
+          </CardHeader>
+          <CardContent>
+            <div className="text-muted-foreground flex space-x-4 text-xs">
+              <span>Last Updated {dayjs(updatedAt).fromNow()}</span>
+            </div>
+          </CardContent>
+        </Card>
+      </Link>
+
+      <span className="absolute right-0 top-0 mr-6 mt-6">{action}</span>
+    </div>
   );
 }
