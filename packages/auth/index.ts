c8351c9713063359001692e6f9c0b041b8946c1e
Kyle Wong <wongyeowkin@gmail.com>
2023-10-04 23:08:28 +0800

    chore: bump next-auth to 0.0.0-manual.ffd05533

--- packages/auth/index.ts
+++ packages/auth/index.ts
@@ -1,9 +1,10 @@
 import Google from "@auth/core/providers/google";
 import type { DefaultSession } from "@auth/core/types";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import NextAuth from "next-auth";
+
 import { db, tableCreator } from "@dashbored/db";
 import type { Workspaces } from "@dashbored/db/schema/workspace";
-import NextAuth from "next-auth";
 
 import { env } from "./env.mjs";
 
